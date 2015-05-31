7244ab70854448f986a1966a94b37442f5d36776
Ming Zhao <mzhao@luminatewireless.com>
2015-05-31 14:16:30 -0700

    Add script to build grte and package them.

---
+++ build_grte.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This runs inside docker container as root to build GRTE
+
+set -ex
+apt-get update
+apt-get install -y texinfo texi2html
+
+GRTE_PREFIX=$1
+GRTE_TMPDIR=$2
+mkdir ${GRTE_PREFIX}
+
+rm -rf grte/sources/*
+./grte/prepare-sources.sh
+./grte/grte-build ${GRTE_PREFIX} ${GRTE_TMPDIR}
+./grte/grte-package ${GRTE_PREFIX} ${GRTE_TMPDIR}
+
+echo "Packages are in ${GRTE_TMPDIR}/results/debs"
+bash
